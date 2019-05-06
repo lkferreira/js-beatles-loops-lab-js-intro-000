9a4cf51ec93dd2b2b219a527ec2e0ca7aefa5524
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-06 02:44:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (var i = 1; i < musicians.length + 1; i++) {
+    array.push(`${musicians[i-1]} plays ${instruments[i-1]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(array) {
+  var i = 0
+  while (i < array.length + 1) {
+    array[i] = `${array[i]}!!!`
+    i++
+  }
+  return array
+}
